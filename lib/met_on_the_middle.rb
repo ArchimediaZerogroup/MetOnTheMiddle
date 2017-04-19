46c1a61fa791783444fb7b312417a0a0d69b8f98
Marino <marinobonetti@gmail.com>
2017-04-19 12:47:29 +0200

    riorganizzazione module e version bump

--- lib/met_on_the_middle.rb
+++ lib/met_on_the_middle.rb
@@ -1,16 +1,3 @@
-Dir[File.expand_path('../met_on_the_middle/readers/*.rb', __FILE__)].each {|file| require file}
-Dir[File.expand_path('../met_on_the_middle/senders/*.rb', __FILE__)].each {|file| require file}
-require 'met_on_the_middle/version'
-require 'met_on_the_middle/root_middleware'
-require 'met_on_the_middle/configuration'
-require 'met_on_the_middle/request'
-require 'met_on_the_middle/collector'
-require 'met_on_the_middle/tracker'
-require 'met_on_the_middle/worker'
-
-
-
-require 'met_on_the_middle/railtie' if defined?(Rails)
 module MetOnTheMiddle
   # Your code goes here...
 
@@ -27,3 +14,17 @@ module MetOnTheMiddle
   end
 
 end
+
+Dir[File.expand_path('../met_on_the_middle/readers/*.rb', __FILE__)].each {|file| require file}
+Dir[File.expand_path('../met_on_the_middle/senders/*.rb', __FILE__)].each {|file| require file}
+require 'met_on_the_middle/version'
+require 'met_on_the_middle/root_middleware'
+require 'met_on_the_middle/configuration'
+require 'met_on_the_middle/request'
+require 'met_on_the_middle/collector'
+require 'met_on_the_middle/tracker'
+require 'met_on_the_middle/worker'
+
+
+
+require 'met_on_the_middle/railtie' if defined?(Rails)

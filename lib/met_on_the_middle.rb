dbc82ba369f00949b6cffffa315ff174124db549
Marino <marinobonetti@gmail.com>
2017-04-19 13:34:47 +0200

    modifica load elementi

--- lib/met_on_the_middle.rb
+++ lib/met_on_the_middle.rb
@@ -1,4 +1,27 @@
 module MetOnTheMiddle
+
+end
+
+require 'met_on_the_middle/readers/base'
+require 'met_on_the_middle/readers/database_time'
+require 'met_on_the_middle/readers/request_count'
+require 'met_on_the_middle/readers/total_time'
+require 'met_on_the_middle/readers/view_time'
+
+require 'met_on_the_middle/senders/base'
+require 'met_on_the_middle/senders/file_system'
+require 'met_on_the_middle/senders/zabbix'
+
+require 'met_on_the_middle/version'
+require 'met_on_the_middle/root_middleware'
+require 'met_on_the_middle/configuration'
+require 'met_on_the_middle/request'
+require 'met_on_the_middle/collector'
+require 'met_on_the_middle/tracker'
+require 'met_on_the_middle/worker'
+
+
+module MetOnTheMiddle
   # Your code goes here...
 
   class << self
@@ -14,17 +37,6 @@ module MetOnTheMiddle
   end
 
 end
-
-Dir[File.expand_path('../met_on_the_middle/readers/*.rb', __FILE__)].each {|file| require file}
-Dir[File.expand_path('../met_on_the_middle/senders/*.rb', __FILE__)].each {|file| require file}
-require 'met_on_the_middle/version'
-require 'met_on_the_middle/root_middleware'
-require 'met_on_the_middle/configuration'
-require 'met_on_the_middle/request'
-require 'met_on_the_middle/collector'
-require 'met_on_the_middle/tracker'
-require 'met_on_the_middle/worker'
-
 
 
 require 'met_on_the_middle/railtie' if defined?(Rails)
